9177107911e9f056b6b8961e92214e65ef02d91e
Ebru Ozdil <ebruozdil99@gmail.com>
2022-09-28 21:48:00 +0300

    Java da Input kullanımı, girilen iki sayıyı toplama, kullanıcı verilerini alma

---
+++ src/GirilenIkiSayininToplami.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class GirilenIkiSayininToplami {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int a, b;
+
+        System.out.println("A sayısını giriniz : ");
+        a = input.nextInt();
+
+        System.out.println("B sayısını giriniz : ");
+        b = input.nextInt();
+
+        System.out.println("A Sayısı : " + a);
+        System.out.println("B Sayısı : " + b);
+        int i = a + b;
+        System.out.println("Toplamları : "+  i);
+    }
+}
