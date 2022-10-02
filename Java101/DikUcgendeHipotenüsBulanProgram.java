9d4d4d805336e3cf96af5f40754303abf6c2931c
Ebru Ozdil <ebruozdil99@gmail.com>
2022-10-02 22:31:33 +0300

    Dik üçgende hipotenüs

---
+++ Java101/DikUcgendeHipotenüsBulanProgram.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class DikUcgendeHipotenüsBulanProgram {
+    public static void main(String[] args) {
+    double kenar1 , kenar2, hipotenüs;
+
+    Scanner input = new Scanner(System.in);
+    
+    System.out.println("1.Kenarı Giriniz : ");
+    kenar1 = input.nextDouble();
+    System.out.println("2.Kenari Giriniz : ");
+    kenar2 = input.nextDouble();
+
+    hipotenüs = Math.sqrt((kenar1*kenar1)+(kenar2*kenar2));
+    System.out.println("Hipotenüs = "+hipotenüs);
+    }
+}
