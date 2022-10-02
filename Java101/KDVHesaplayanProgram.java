4b3d715118d995ac7ee5820ca33e7cce663c1e24
Ebru Ozdil <ebruozdil99@gmail.com>
2022-10-02 14:54:27 +0300

    Kdv düzen

--- Java101/KDVHesaplayanProgram.java
+++ Java101/KDVHesaplayanProgram.java
@@ -2,16 +2,14 @@ import java.util.Scanner;
 /*
  KDV Tutarı Hesaplayan Program
 Java ile kullanıcıdan alınan para değerinin KDV'li fiyatını ve KDV tutarını hesaplayıp ekrana bastıran programı yazın.
-
 (Not : KDV tutarını 18% olarak alın)
-
 KDV'siz Fiyat = 10;
-
 KDV'li Fiyat = 11.8;
-
 KDV tutarı = 1.8;
- */
 
+Ödev
+Eğer girilen tutar 0 ve 1000 TL arasında ise KDV oranı %18 , tutar 1000 TL'den büyük ise KDV oranını %8 olarak KDV tutarı hesaplayan programı yazınız.
+ */
 public class KDVHesaplayanProgram {
     public static void main(String[] args) {
         double tutar,kdvOran, kdvTutar, kdvliTutar;
@@ -20,7 +18,7 @@ public class KDVHesaplayanProgram {
         System.out.println("Para değerini giriniz:");
         tutar = input.nextDouble();
         
-        kdvOran=(tutar<=1000)?0.18:0.08;
+        kdvOran=(tutar<=1000)?0.18:0.08;  //Ödev kısmı 
         kdvTutar =tutar * kdvOran;
         kdvliTutar = tutar + kdvTutar ;
 
@@ -32,7 +30,3 @@ public class KDVHesaplayanProgram {
     }
 }
 
-/*
-  Ödev
-Eğer girilen tutar 0 ve 1000 TL arasında ise KDV oranı %18 , tutar 1000 TL'den büyük ise KDV oranını %8 olarak KDV tutarı hesaplayan programı yazınız.
- */
