eb529cc7ec5b5d69c696ca291e1f0fdf4f1f2652
Ebru Ozdil <ebruozdil99@gmail.com>
2022-10-05 02:30:26 +0300

    Vucüt kitle indeksi hesaplayan program

---
+++ Java101/VucutKitleIndeksiHesaplayanProgram.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class VucutKitleIndeksiHesaplayanProgram {
+    public static void main(String[] args) {
+        double boy, kilo, indeks;
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Lütfen boyunuzu (metre cinsinden) giriniz : ");
+        boy= input.nextDouble();
+
+        Scanner input2 = new Scanner(System.in);
+        System.out.print("Lütfen kilonuzu giriniz : ");
+        kilo= input2.nextDouble();
+
+        //Vücut Kitle İndeksi Hesaplama Formülü : Kilo (kg) / Boy(m) * Boy(m)
+
+        indeks = kilo / (boy * boy) ;
+        System.out.println("Vücut Kitle İndeksiniz : " + indeks);
+
+    }
+}
