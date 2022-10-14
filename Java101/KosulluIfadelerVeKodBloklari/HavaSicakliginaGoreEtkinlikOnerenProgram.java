01c891dd969afaea9afddd99cb9837430d9f4841
Ebru Ozdil <ebruozdil99@gmail.com>
2022-10-14 20:04:48 +0300

    Sıcaklığa göre etkinlik önerme ödev kısmı

--- Java101/KosulluIfadelerVeKodBloklari/HavaSicakliginaGoreEtkinlikOnerenProgram.java
+++ Java101/KosulluIfadelerVeKodBloklari/HavaSicakliginaGoreEtkinlikOnerenProgram.java
@@ -1,26 +1,28 @@
+/*Sıcaklık 5'dan küçük ise "Kayak" yapmayı öner.
+ Sıcaklık 5 ve 15 arasında ise "Sinema" etkinliğini öner. 
+ Sıcaklık 15 ve 25 arasında ise "Piknik" etkinliğini öner. 
+ Sıcaklık 25'ten büyük ise "Yüzme" etkinliğini öner. 
+ Ödev 
+ Aynı örnek üzerinden if koşulları başka hangi şekilde oluşturulabilirdi farklı çözüm yolları bulunuz. */
 package KosulluIfadelerVeKodBloklari;
+
 import java.util.Scanner;
 
 public class HavaSicakliginaGoreEtkinlikOnerenProgram {
     public static void main(String[] args) {
-        int heat ;
-        
+        int heat;
         Scanner input = new Scanner(System.in);
         System.out.println("Hava sıcaklığı kaç derece? : ");
-        heat=input.nextInt();
-
-        if (heat<5){
+        heat = input.nextInt();
+        if (heat < 5) {
             System.out.println("Kayak yapabilirsiniz.");
-        }else if(heat<25){
-            if (heat <= 15){
-                System.out.println("Sinemaya gidebilirsiniz.");
-            }
-            if(heat>=10){
-                System.out.println("Pikniğe gidebilirsiniz.");
-            }
-        }else{
+        } else if (heat < 25) {
+            System.out.println("Sinemaya gidebilirsiniz.");
+        }
+        if (heat <= 25 && heat > 15) {
+            System.out.println("Pikniğe gidebilirsiniz.");
+        } else if (heat > 25) {
             System.out.println("Yüzmeye gidebilirsiniz.");
         }
     }
-    
 }
