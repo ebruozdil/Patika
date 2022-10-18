3d5d9fc1099b28f4368363032a654b6ebfe0005b
Ebru Ozdil <ebruozdil99@gmail.com>
2022-10-19 00:31:13 +0300

    Girilen sayıya kadr 3ve4e bölünen saylarn ortlamsı

---
+++ Java101/Donguler/GirilenSayiyaKadar3Ve4eBolunenSayilarinOrtalamasi.java
@@ -0,0 +1,25 @@
+package Donguler;
+
+import java.util.Scanner;
+
+public class GirilenSayiyaKadar3Ve4eBolunenSayilarinOrtalamasi {
+    public static void main(String[] args) {
+        int k , t=0 ,total=0 ;
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Bir Sayı Giriniz : ");
+        k= input.nextInt();
+
+        
+        for (int i=1; i<=k; i++ ){
+            if(i%3==0 && i%4==0 ){
+                t++;
+                System.out.println(i);
+                total+=i;  
+                
+                System.out.println(t);
+            }
+        }
+        System.out.println(k+"ya kadar olan sayılardan 3 e ve 4 e bölünen sayıların ortalaması: "+total/t);
+    }
+}
