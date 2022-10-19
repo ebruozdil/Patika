8bec2aeaecd8fc57c13991cc46a17d492d2017c3
Ebru Ozdil <ebruozdil99@gmail.com>
2022-10-19 11:30:55 +0300

    girilen sayıya kadar olan 4 ve 5'in kuvvetlerini yazdıran program

---
+++ Java101/Donguler/GirilenSayiyaKadar4Ve5inKuvvetleriniBulanPrgram.java
@@ -0,0 +1,25 @@
+package Donguler;
+
+import java.util.Scanner;
+
+public class GirilenSayiyaKadar4Ve5inKuvvetleriniBulanPrgram {
+    public static void main(String[] args) {
+        int n;
+        Scanner input= new Scanner(System.in);
+        System.out.print("Sınır sayısını giriniz: ");
+        n= input.nextInt();
+
+        System.out.println("4'ün kuvvetleri: ");
+        for (int i=1; i<=n ; i*=4){
+                System.out.println(i);
+        }
+
+        System.out.println("5'in kuvvetleri: ");
+        for (int i=1; i<=n ; i*=5){
+            System.out.println(i);
+        }
+    }
+}
+/*Ödev  Patika.dev
+Java döngüler ile girilen sayıya kadar 
+olan 4 ve 5'in kuvvetlerini ekrana yazdıran programı yazıyoruz. */
