86dea2106e01842f2d252b2677d4b79bc8b51892
Ebru Ozdil <ebruozdil99@gmail.com>
2022-10-19 11:44:21 +0300

    faktöriyel hesaplayan program

---
+++ Java101/Donguler/FaktoriyelHasaplayanProgram.java
@@ -0,0 +1,19 @@
+package Donguler;
+
+import java.util.Scanner;
+
+public class FaktoriyelHasaplayanProgram {
+    public static void main(String[] args) {
+        // n! = 1* 2 * 3 * 4 *...* n
+
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Faktöriyel Sayısı : ");
+        int n = scan.nextInt();
+        int total=1;
+
+        for(int i=1; i<=n ; i++){
+            total = total*i;
+        }
+        System.out.println(n+" Faktöriyel = "+total);
+    }
+}
