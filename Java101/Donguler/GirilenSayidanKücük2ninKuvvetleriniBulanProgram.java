b8a04f30c4d331ce61f7f32da18bf3044b966400
Ebru Ozdil <ebruozdil99@gmail.com>
2022-10-19 11:11:26 +0300

    girlen sayıya kadar 2nin kuvvetlerni yazdran prgrm

---
+++ Java101/Donguler/GirilenSayidanKücük2ninKuvvetleriniBulanProgram.java
@@ -0,0 +1,17 @@
+package Donguler;
+
+import java.util.Scanner;
+
+public class GirilenSayidanKücük2ninKuvvetleriniBulanProgram {
+    public static void main(String[] args) {
+        int n;
+        Scanner input = new Scanner(System.in);
+        System.out.print("Sınır sayısını giriniz : ");
+        n = input.nextInt();
+
+        for (int i=1; i<=n; i*=2){
+            System.out.println(i);
+        }
+    }
+}
+/*Java döngüler ile girilen sayıya kadar olan 2'nin kuvvetlerini ekrana yazdıran programı yazıyoruz.*/
