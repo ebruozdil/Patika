d1c54cea18eec7d4411868d19fe526b0c3279995
Ebru Ozdil <ebruozdil99@gmail.com>
2022-10-19 09:22:37 +0300

    tek bir sayı girilene kadar kullanıcıdan girişleri kabul eden ve girilen değerlerden çift ve 4'ün katları olan sayıları toplayan prgrm

---
+++ Java101/Donguler/TekSayiGireneKadarCiftVe4unKatıOlanSyilariToplayanProgram.java
@@ -0,0 +1,22 @@
+package Donguler;
+import java.util.Scanner;
+
+public class TekSayiGireneKadarCiftVe4unKatıOlanSyilariToplayanProgram {
+    public static void main(String[] args) {
+        int n,total=0;
+        Scanner input = new Scanner(System.in);
+
+        do{
+            System.out.print("Bir Sayı Giriniz: ");
+            n= input.nextInt();
+
+            if(n%2==0 && n%4==0 ){
+                total+=n;
+            }
+        }while(n%2==0);
+        System.out.println(total);
+    }
+}
+/*Ödev
+Java döngüler ile tek bir sayı girilene kadar kullanıcıdan girişleri kabul eden 
+ve girilen değerlerden çift ve 4'ün katları olan sayıları toplayıp ekrana basan programı yazıyoruz. */
