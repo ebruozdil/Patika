99ab305cc973601a231babcc5a080aaf1e991661
Ebru Ozdil <ebruozdil99@gmail.com>
2022-10-22 19:04:22 +0300

    harmonik seri

---
+++ Java101/Donguler/HarmonikSeri.java
@@ -0,0 +1,15 @@
+package Donguler;
+import java.util.Scanner;
+public class HarmonikSeri {
+//Java ile girilen sayının harmonik serisini bulan program yazacağız.
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Bir sayı giriniz : ");
+        int number = input.nextInt();
+        double result =0.0 ;
+        for (double i = 1;i<=number ; i++){
+            result += (1/i);
+        }
+        System.out.println(result);
+    }
+}
