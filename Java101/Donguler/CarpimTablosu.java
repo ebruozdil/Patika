6d7920db7effd6c069fab8af3be3694f19a86415
Ebru Ozdil <ebruozdil99@gmail.com>
2022-10-26 23:51:47 +0300

    Çarpım tablosu

---
+++ Java101/Donguler/CarpimTablosu.java
@@ -0,0 +1,14 @@
+package Donguler;
+import java.util.Scanner;
+public class CarpimTablosu {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("10 luk tabloda Çarpılacak sayıyı giriniz: ");
+        int N = scanner.nextInt();
+
+        for(int i=1; i<=10; i++){
+            int result = N * i;
+            System.out.println(N+ " x "+i+" = "+result);
+        }
+    }
+}
