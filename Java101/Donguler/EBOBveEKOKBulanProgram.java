21736f949ba264bcd2616e02911e644e12fdbf0f
Ebru Ozdil <ebruozdil99@gmail.com>
2022-10-26 07:13:42 +0300

    ebob ekok

--- Java101/Donguler/EBOBveEKOKBulanProgram.java
+++ Java101/Donguler/EBOBveEKOKBulanProgram.java
@@ -1,13 +1,11 @@
 package Donguler;
-
 import java.util.Scanner;
-
 public class EBOBveEKOKBulanProgram {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.println("n1 sayısını giriniz : ");
+        System.out.print("n1 sayısını giriniz : ");
         int n1 = input.nextInt();
-        System.out.println("n2 sayısını giriniz : ");
+        System.out.print("n2 sayısını giriniz : ");
         int n2 = input.nextInt();
         int ebob = 1;
 //ebob
@@ -22,11 +20,20 @@ public class EBOBveEKOKBulanProgram {
         for (int k=n1; k>=1; k--){
             if (n1%k==0 && n2%k==0){
                 ebob=k;
-                System.out.println(ebob);
+                System.out.println(n1+" ve "+n2+" sayılarının EBOB'u : "+ebob);
                 break;
             }
         }
 
 //ekok
+        for (int i=1 ; i<=(n1 * n2); i++){
+            if (i%n1==0 && i%n2==0){
+                System.out.println(n1+" ve "+n2+" sayılarının EKOK'u "+i);
+                break;
+            }
+        }
+//formülden ekok :
+
+        System.out.println((n1*n2) / ebob);
     }
 }
