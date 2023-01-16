b54e148037903008bbba2abe82c43989f89c60ed
ebruozdil <ebruozdil99@gmail.com>
2023-01-16 19:20:59 +0300

    Çok boyutlu dizilerle * ile ekrana harf yazma

---
+++ Java101/Diziler/PratiklerVeOdevler/letterA.java
@@ -0,0 +1,25 @@
+package Diziler.PratiklerVeOdevler;
+
+public class letterA {
+    public static void main(String[] args) {
+        String[][] letter = new String [6][4];
+
+        for (int i=0; i<letter.length; i++){
+            for ( int j=0; j<letter[i].length; j++){
+                if(i==0 || i==2){
+                    letter[i][j]= " * ";
+                }else if(j==0 || j==3){
+                    letter[i][j]= " * ";
+                }else{
+                    letter[i][j]="   ";
+                }
+            }
+        }
+        for (String[] row : letter ){
+            for (String column : row){
+                System.out.print(column);
+            }
+            System.out.println();
+        }
+    }
+}
