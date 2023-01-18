2f3f41edf50b8480b64d0575999b8574cd689012
ebruozdil <ebruozdil99@gmail.com>
2023-01-18 17:35:06 +0300

    transpoze different way

--- Java101/Diziler/PratiklerVeOdevler/MatrisTranspozunuBulma.java
+++ Java101/Diziler/PratiklerVeOdevler/MatrisTranspozunuBulma.java
@@ -27,3 +27,27 @@ public class MatrisTranspozunuBulma {
         printMatris(transpoze);
     }
 }
+
+
+
+
+
+
+/* public static void main(String[] args) {
+    int[][] a = { { 2, 3, 4 },
+            { 5, 6, 4 } };
+    int[][] aT = new int[a[0].length][a.length];
+
+    for (int row = 0; row < a.length; row++) {
+        for (int col = 0; col < a[row].length; col++) {
+            aT[col][row] = a[row][col];
+        }
+    }
+
+    for (int i = 0; i < aT.length; i++) {
+        for (int j = 0; j < aT[i].length; j++) {
+            System.out.print(aT[i][j] + "   ");
+        }
+        System.out.println();
+    }
+} */
